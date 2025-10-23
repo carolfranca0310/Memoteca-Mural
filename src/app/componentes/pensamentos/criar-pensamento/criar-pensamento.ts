5a19cb0478e9864855b38bfe9b300b1403be072d
Carol Araujo <carolfranca0310@gmail.com>
2025-10-23 17:29:01 -0300

    feat: implementar vinculação de dados e ações nos botões do formulário

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-criar-pensamento',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './criar-pensamento.html',
   styleUrl: './criar-pensamento.css'
 })
 export class CriarPensamento {
+
+  pensamento: Pensamento = {
+    id: 1,
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado!")
+  }
+  cancelar() {
+    alert("Pensamento cancelado!")
+  }
 
 }
