2f0a75003fb0f13b1fab9875bc7d80ff8aa5e097
Carol Araujo <carolfranca0310@gmail.com>
2025-10-23 17:25:32 -0300

    feat: configurar rotas de pensamentos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CriarPensamento } from './componentes/pensamentos/criar-pensamento/criar-pensamento';
+import { ListarPensamentos } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',redirectTo: 'listarPensamento', pathMatch: 'full'},
+  { path: 'criarPensamento', component: CriarPensamento },
+  { path: 'listarPensamento', component: ListarPensamentos },
+];
