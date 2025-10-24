ec3784d5023a0236da97b5cc3ba10fd3a5bdf3ef
Carol Araujo <carolfranca0310@gmail.com>
2025-10-24 14:30:48 -0300

    feat(routes): incluir rotas para componentes de exclusão e edição de pensamentos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { CriarPensamento } from './componentes/pensamentos/criar-pensamento/criar-pensamento';
 import { ListarPensamentos } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento';
 
 export const routes: Routes = [
   { path: '',redirectTo: 'listarPensamento', pathMatch: 'full'},
   { path: 'criarPensamento', component: CriarPensamento },
   { path: 'listarPensamento', component: ListarPensamentos },
+  { path: 'pensamentos/excluirPensamento/:id', component: ExcluirPensamentoComponent },
+  { path: 'pensamentos/editarPensamento/:id', component: EditarPensamentoComponent },
 ];
